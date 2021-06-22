d72f325c67b57d77ecf535d39ab2e3bddb61e689
Glitch (glitch-hello-website) <none>
2021-06-22 13:50:16 +0000

    🍠🐮 Checkpoint ./script.js:311760/6334 ./index.html:311760/5186 ./bg.js:311760/131085

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-const btn = document.querySelector("#save");
+const app = new Vue({
+  el: "#app",
+  data: {
+    name: "Phạm Huy Hoàng",
+    url: "https://linktr.ee/codedao",
+    qrCode: undefined
+  },
+  mounted: function() {
+    const qrCode = new QRCode("qr-code", {
+      text: this.url,
+      width: 300,
+      height: 300
+    });
+    this.qrCode = qrCode;
+  },
+  methods: {
+    exportCard: async () => {
+      const dataUrl = await domtoimage.toPng(document.querySelector("#card"))
+      
+        const img = new Image();
+        img.src = dataUrl;
+        document.body.appendChild(img);
 
-btn.addEventListener("click", () => {
-  domtoimage.toPng(document.querySelector("#card")).then(function(dataUrl) {
-    const img = new Image();
-    img.src = dataUrl;
-    document.body.appendChild(img);
-
-    const link = document.createElement("a");
-    link.download = "taotap-card.jpeg";
-    link.href = dataUrl;
-    link.click();
-  });
-});
-
-const qrcode = new QRCode("qr-code", {
-  text: "https://linktr.ee/codedao",
-  width: 300,
-  height: 300
+        const link = document.createElement("a");
+        link.download = "taotap-card.jpeg";
+        link.href = dataUrl;
+        link.click();
+      
+    }
+  }
 });
