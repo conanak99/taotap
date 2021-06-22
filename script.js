b4dffdb1a1f3214d17a5a8372e9a746a1629b516
Glitch (glitch-hello-website) <none>
2021-06-22 12:06:22 +0000

    📟🐷 Checkpoint ./index.html:311760/10557 ./script.js:311760/1428 ./style.css:311760/4250

--- script.js
+++ script.js
@@ -1,27 +1,13 @@
-/*
-This is your site JavaScript code - you can add interactivity and carry out processing
-- Initially the JS writes a message to the console, and rotates a button you can add from the README
-*/
+const btn = document.querySelector('#save')
 
-// Print a message in the browser's dev tools console each time the page loads
-// Use your menus or right-click / control-click and choose "Inspect" > "Console"
-console.log("Hello 🌎");
-
-/* 
-Make the "Click me!" button rotate when the visitor clicks it:
-- First add the button to the page by following the "Next steps" in the README
-*/
-const btn = document.querySelector("button"); // Get the button from the page
-// Detect clicks on the button
-if (btn) {
-  btn.onclick = function() {
-    // The JS works in conjunction with the 'rotated' code in style.css
-    btn.classList.toggle("rotated");
-  };
-}
-
-// This is a single line JS comment
-/*
-This is a comment that can span multiple lines 
-- use comments to make your own notes!
-*/
+btn.addEventListener('click', () => {
+  domtoimage.toPng(document.querySelector('#card'))
+    .then(function (dataUrl) {
+        var img = new Image();
+        img.src = dataUrl;
+        document.body.appendChild(img);
+    })
+    .catch(function (error) {
+        console.error('oops, something went wrong!', error);
+    });
+})
