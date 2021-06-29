0940df72536fa4ca72c5db799ca2767cc2df2738
Glitch (glitch-hello-website) <none>
2021-06-29 13:37:19 +0000

    🍎🚟 Checkpoint ./script.js:311760/689

--- script.js
+++ script.js
@@ -124,6 +124,18 @@ const app = new Vue({
       const HEIGHT = 54 * RATIO;
       doc.addImage(img, "JPEG", 10, 10, WIDTH, HEIGHT);
       doc.addImage(img, "JPEG", 10, 70, WIDTH, HEIGHT);
+      
+      console.log(doc.getFontList())
+      
+      doc.setFontSize(16)
+      doc.setFont('courier', "bold")
+      doc.text(`Chon "Fit to Paper khi in nhe :3" `, 10, 150)
+      
+      doc.setLineWidth(3)
+      doc.line(0,0, 210, 0)
+      doc.line(0,0, 0, 297)
+      doc.line(210,0, 210, 297)
+      doc.line(0,297, 210, 297)
       doc.save("taotap.pdf");
     },
     setBackground: function(bg) {
